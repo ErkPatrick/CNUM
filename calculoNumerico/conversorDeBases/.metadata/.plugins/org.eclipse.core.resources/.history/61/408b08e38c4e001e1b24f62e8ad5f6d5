7685f59e89e711cd8514890afa7b6380cd5f936c
ErkPatrick <erick-patrickfreitas@outlook.com>
2023-10-03 18:55:46 -0300

    Implementações de cálculo numérico

---
+++ calculoNumerico/conversorDeBases/.metadata/.plugins/org.eclipse.core.resources/.history/61/408b08e38c4e001e1b24f62e8ad5f6d5
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module ConversorBases {
+}
---
+++ calculoNumerico/conversorDeBases/ConversorBases/src/conversor/Principal.java
@@ -0,0 +1,8 @@
+package conversor;
+
+public class Principal {
+	public static void main(String[] args) {
+		Binario teste = new Binario();
+		System.out.println(teste.conversorBinario("A76B4", "hexadecimal"));
+	}
+}
